6218eb365099635b0a5b175c3d452341cb9c7daa
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-07-31 00:47:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let mobile_btn= document.getElementById("mobile_btn")
+let mobail_menu= document.getElementById("mobail_menu")
+
+mobile_btn.addEventListener("click",()=>{
+  mobail_menu.classList.toggle("hidden");
+})
+
+// ai khane toggole dara menu on off
